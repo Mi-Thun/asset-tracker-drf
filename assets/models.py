5fbe260e498cb326c9136d9763c9694f58676d3b
Mithun <mohsenulkabirmi8486@gmail.com>
2024-05-09 22:48:39 +0600

    model representaion, migration file added

--- assets/models.py
+++ assets/models.py
@@ -5,9 +5,17 @@ class Company(models.Model):
     # Assume company name is unique
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
+    # Assume User is already created and we are using it here
+    # Here we use default User model from Django
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, related_name='employees', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 class Device(models.Model):
     # Assume there are only 3 option. If want to add more, add here. 
@@ -21,6 +29,9 @@ class Device(models.Model):
     type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     serial_number = models.CharField(max_length=255, unique=True)
     condition = models.TextField()
+
+    def __str__(self):
+        return self.type + ' - ' + self.serial_number
 
 class DeviceAssignment(models.Model):
     # Assume that each device can only be assigned to one employee at a time
