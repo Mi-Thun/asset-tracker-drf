8fef9019e4ff6442a70d7445f9d7654c6404d26a
Mithun <mohsenulkabirmi8486@gmail.com>
2024-05-09 22:25:02 +0600

    views  added

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Device, DeviceAssignment, Employee, Company
+from .serializers import DeviceSerializer, AssignmentSerializer, EmployeeSerializer, CompanySerializer
 
-# Create your views here.
+class DeviceViewSet(viewsets.ModelViewSet):
+    # By default, this will allow GET, POST, PUT, DELETE
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class AssignmentViewSet(viewsets.ModelViewSet):
+    # By default, this will allow GET, POST, PUT, DELETE
+    queryset = DeviceAssignment.objects.all()
+    serializer_class = AssignmentSerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    # By default, this will allow GET, POST, PUT, DELETE
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    # By default, this will allow GET, POST, PUT, DELETE
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
